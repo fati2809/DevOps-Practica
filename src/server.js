17c9031c660d8dc360b103820f430daed931039f
fati2809 <2023171042@uteq.edu.mx>
2026-03-02 11:23:24 -0600

    Create server.js

---
+++ src/server.js
@@ -0,0 +1,10 @@
+const app = require('./app');
+const PORT = process.env.PORT || 3000;
+
+// Levanta el servidor Express
+const server = app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+// Exporta el server por si quieres usarlo en tests
+module.exports = server;
